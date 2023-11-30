b2438762df1e9a8e7be71dbb0b3887a017f6c723
Wonuuuuu <gordon44567@gmail.com>
2023-11-30 13:10:26 +0800

    fix createStadium fig problem

--- frontend/src/provider/CRUD/createStadium.js
+++ frontend/src/provider/CRUD/createStadium.js
@@ -10,25 +10,29 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
-
 export default function CreateStadium() {
   const navigate = useNavigate();
-  const[ image, setImage ] = useState()
+  const [image, setImage] = useState();
+  const handleImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = URL.createObjectURL(e.target.files[0]);
+      setImage(img);
+    }
+  };
   function showMyImage(fileInput) {
     var files = fileInput.files;
-      console.log(files);
-      for (var i = 0; i < files.length; i++) {           
-          var file = files[i];
-          console.log(file.name);
-          var imageType = /image.*/;     
-          if (!file.type.match(imageType)) {
-              continue;
-          }           
-          console.log(file)
-          //$("#banner_name").text(file.name);
-
+    console.log(files);
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      console.log(file.name);
+      var imageType = /image.*/;
+      if (!file.type.match(imageType)) {
+        continue;
       }
-}
+      console.log(file);
+      //$("#banner_name").text(file.name);
+    }
+  }
   return (
     <div>
       <h1> 新增場地資訊 </h1>
@@ -50,49 +54,28 @@ export default function CreateStadium() {
             <Card sx={{ width: "70vw", margin: "auto" }}>
               <Grid container>
                 <Grid item xs={6}>
-                  <CardMedia
-                    component="img"
-                    src={image}
-                    alt="Stadium"
-                  />
-                  <TextField 
+                  <CardMedia component="img" src={image} alt="Stadium" />
+                  <TextField
                     type="file"
-                    onClick={(e)=>setImage(e.target.value)}
-                    onChange={(e)=>{
-                        console.log(e.target.value)
-                        setImage(e.target.value)}}
-                    />
-                    {/* <Button variant="contained" color="primary" component="span">
+                    onChange={(e) => {
+                      handleImageChange(e);
+                      showMyImage(e.target);
+                    }}
+                  />
+                  {/* <Button variant="contained" color="primary" component="span">
                         Upload
                     </Button> */}
                 </Grid>
                 <Grid item xs={6}>
                   <CardContent>
-                    <Typography>
-                        球場名稱
-                    </Typography>
-                    <TextField
-                        fullWidth
-                        size="small"
-                    />
-                    <Typography>
-                        球場地址
-                    </Typography>
-                    <TextField
-                        fullWidth
-                        size="small"
-                    />
-                    <Typography>
-                        最大使用人數
-                    </Typography>
-                    <TextField
-                        fullWidth
-                        size="small"
-                    />
+                    <Typography>球場名稱</Typography>
+                    <TextField fullWidth size="small" />
+                    <Typography>球場地址</Typography>
+                    <TextField fullWidth size="small" />
+                    <Typography>最大使用人數</Typography>
+                    <TextField fullWidth size="small" />
                   </CardContent>
-            
                 </Grid>
-                
               </Grid>
             </Card>
           </Box>
