3a657c3ecb69c2783251eb5ac1341230f94cda75
李書成 <sean022502@gmail.com>
2023-12-05 01:13:14 +0800

    conflict

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -1,9 +1,27 @@
 import db from './connection_db.js';
 
 // get courts appointment query
-export const getCourtsAppointmentQuery = () => {
+export const getCourtsAppointmentQuery = (params) => {
+    console.log(params);
+    const name = params.name !== 'ALL' ? params.name : null;
+    const ball = params.ball !== 'ALL' ? params.ball : null;
+    const location = params.location !== 'ALL' ? params.location : null;
+    const date = params.date !== 'ALL' ? params.date : null;
+    const page = params.page || 1;
+    const limit = 10;
+    const offset = (page - 1) * limit;
+
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM STADIUM.COURT INNER JOIN STADIUM.APPOINTMENT ON COURT.court_id = APPOINTMENT.court_id', (error, results) => {
+        db.query(`SELECT COURT.* FROM STADIUM.COURT 
+        INNER JOIN STADIUM.APPOINTMENT ON COURT.court_id = APPOINTMENT.court_id
+        INNER JOIN STADIUM.APPOINTMENT_TIME ON APPOINTMENT.appointment_id = APPOINTMENT_TIME.appointment_id
+        INNER JOIN USER ON COURT.admin_id = USER.user_id
+        WHERE 
+            (USER.name = ? OR ? IS NULL) 
+            AND (ball = ? OR ? IS NULL) 
+            AND (location = ? OR ? IS NULL)
+            AND (date = ? OR ? IS NULL)
+            LIMIT ? OFFSET ?`, [name, name, ball, ball, location, location, date, date, limit, offset],(error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -20,9 +38,10 @@ export const getCourtsAppointmentDetailsQuery = (data) => {
 
     return new Promise((resolve, reject) => {
 
-        db.query(`SELECT start_time, end_time, COUNT(P.appointment_id) AS participant_count FROM STADIUM.APPOINTMENT_TIME AS AT INNER JOIN 
-        STADIUM.APPOINTMENT AS A ON AT.appointment_id = A.appointment_id LEFT JOIN 
-        STADIUM.PARTICIPANT AS P ON A.appointment_id = P.appointment_id
+        db.query(`SELECT start_time, end_time, COUNT(P.appointment_id) AS participant_count, name 
+        FROM STADIUM.APPOINTMENT_TIME AS AT INNER JOIN STADIUM.APPOINTMENT AS A ON AT.appointment_id = A.appointment_id 
+        LEFT JOIN STADIUM.PARTICIPANT AS P ON A.appointment_id = P.appointment_id
+        LEFT JOIN STADIUM.USER AS U ON A.creator_id = U.user_id
         WHERE A.court_id = ? GROUP BY AT.start_time, AT.end_time`, [court_id], (error, results) => {
             if (error) {
                 reject(error);
@@ -33,10 +52,70 @@ export const getCourtsAppointmentDetailsQuery = (data) => {
     });
 }
 
+// get user histories query
+export const getUserHistoriesQuery = () => {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT user_id, name, email, phone FROM USER WHERE role_id = 2', (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+//post court provider
+export const postCourtsProviderQuery = (data) => {
+
+    return new Promise((resolve, reject) => {
+        db.query(`INSERT INTO STADIUM.USER SET ?`, [data], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve("新增完成!");
+            }
+        });
+    });
+}
+
+// put user info by user_id
+export const putUsersByIdQuery = async(data) => {
+
+    const { user_id, ...update_col_dict } = data;
+    console.log(user_id);
+    console.log(update_col_dict);
+
+    return new Promise((resolve, reject) => {
+        db.query('UPDATE STADIUM.USER SET ? WHERE user_id = ?', [update_col_dict, user_id], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve("變更完成!");
+            }
+        });
+    });
+}
+
+// delete user info by user_id
+export const deleteUsersByIdQuery = async(data) => {
+    const user_id = data;
+
+    return new Promise((resolve, reject) => {
+        db.query('DELETE FROM STADIUM.USER WHERE user_id = ?', [user_id], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve("刪除完成!");
+            }
+        });
+    });
+}
+
 // check admin_id 
 export const isAdmin = (data) => {
     return new Promise((resolve, reject) => {
-        const { admin_id } = data
+        const admin_id = data
         db.query('SELECT u.user_id, r.role_id FROM USER u JOIN ROLE_REF r ON u.role_id = r.role_id WHERE u.user_id = ? AND r.role_id = 1;', [admin_id], (error, results) => {
         
             if (error) {
