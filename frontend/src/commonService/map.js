80bc152f941e10310e27cdaf6edd3e33d88e4a23
Wonuuuuu <gordon44567@gmail.com>
2023-12-07 14:39:47 +0800

    map 70 app userprofile

--- frontend/src/commonService/map.js
+++ frontend/src/commonService/map.js
@@ -1,18 +1,18 @@
 import L from "leaflet";
 // import "./map.css"
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect, useState } from "react";
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import PlaceIcon from '@mui/icons-material/Place';
-import IconButton from '@mui/material/IconButton';
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import PlaceIcon from "@mui/icons-material/Place";
+import IconButton from "@mui/material/IconButton";
 
-export default function Map(props){
+export default function Map(props) {
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -22,9 +22,11 @@ export default function Map(props){
   const handleClose = () => {
     setOpen(false);
   };
-  useEffect(()=>{
-    
-    const mymap = L.map("mapid").setView([25.014057657671447, 121.53812819619687], 17); 
+  useEffect(() => {
+    const mymap = L.map("mapid").setView(
+      [25.014057657671447, 121.53812819619687],
+      17
+    );
     const OSMUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
     L.tileLayer(OSMUrl).addTo(mymap);
@@ -38,12 +40,12 @@ export default function Map(props){
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
-      shadowSize: [41, 41]
+      shadowSize: [41, 41],
     });
 
-    const marker = L.marker([25.014057657671447, 121.53812819619687], { icon: greenIcon }).addTo(
-      mymap
-    );
+    const marker = L.marker([25.014057657671447, 121.53812819619687], {
+      icon: greenIcon,
+    }).addTo(mymap);
 
     marker.bindPopup("管理學院").openPopup();
 
@@ -51,22 +53,20 @@ export default function Map(props){
       color: "red",
       fillColor: "#f03",
       fillOpacity: 0.5,
-      radius: 10
+      radius: 10,
     }).addTo(mymap);
-  },[])
-    return (
+  }, []);
+  return (
+    <div id="mapid" style={{ height: "50vh", width: "70vw" }} />
 
-      <div id="mapid"  style={{ height: "50vh", width: "50vw" }} />
+    // <>
 
-      
-        // <>
-        
-        // <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
-        //   <TileLayer
-        //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        //   />
-        // </MapContainer>
-        // </>
-      );
+    // <MapContainer center={[45.4, -75.7]} zoom={12}scrollWheelZoom={false}>
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    // </MapContainer>
+    // </>
+  );
 }
