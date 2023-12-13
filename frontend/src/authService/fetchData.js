30c7ea7d73a2fb6394fc2671ad2d089ea4b1ca07
Wonuuuuu <gordon44567@gmail.com>
2023-12-13 23:25:31 +0800

    #Post appointment Success

--- frontend/src/authService/fetchData.js
+++ frontend/src/authService/fetchData.js
@@ -1,50 +1,55 @@
 import axios from "axios";
 import authHeader from "./authHeader";
 import { Navigate } from "react-router-dom";
-async function getData  (url,page,params) {
-    return await axios.get(url,{
-        headers:authHeader(),
-        params:{
-            page:page,
-            ...(params&&{...params})
-        }
+async function getData(url, page, params) {
+  return await axios
+    .get(url, {
+      headers: authHeader(),
+      params: {
+        page: page,
+        ...(params && { ...params }),
+      },
     })
-    .then((res)=>{return res.data})
-    .catch((error)=>{
-        const status = error.response.status
-        console.log(error)
-        alert(error.response.data)
-        window.location.href="/"
-        return undefined
+    .then((res) => {
+      return res.data;
     })
+    .catch((error) => {
+      const status = error.response.status;
+      console.log(error);
+      alert(error.response.data);
+      window.location.href = "/";
+      return undefined;
+    });
 }
-async function postData(url,body){
-    return await axios.post(url,body,{ headers: authHeader() })
-    .then((res)=>{return res.data})
-    .catch((error)=>{
-        console.log(error)
-        alert("not authorized")
-        window.location.href="/"
-        return []
+async function postData(url, body) {
+  return await axios
+    .post(url, body, { headers: authHeader() })
+    .then((res) => {
+      return res.data;
     })
+    .catch((error) => {
+      console.log(error);
+      alert("not authorized");
+      window.location.href = "/";
+      return [];
+    });
 }
-async function postDateWithImg(url,data,img){
-    var bodyFormData = new FormData();
-    
-    bodyFormData.append("name",data.name);
-    bodyFormData.append("court.jpg",img)
-    axios({
-        method: "post",
-        url: url,
-        data: bodyFormData,
-        headers: {"Content-Type": "multipart/form-data" ,...authHeader()},
-      })
+async function postDateWithImg(url, data, img) {
+  var bodyFormData = new FormData();
+
+  bodyFormData.append("name", data.name);
+  bodyFormData.append("court.jpg", img);
+  axios({
+    method: "post",
+    url: url,
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data", ...authHeader() },
+  });
 }
-const FetchData={
-    getData,
-    postDateWithImg
-}
+const FetchData = {
+  getData,
+  postDateWithImg,
+  postData,
+};
 
-
-
-export default FetchData
+export default FetchData;
