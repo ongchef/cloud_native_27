1486ff6ef2254185e4fa8858581e6be324782107
CloveryPace <128057141+CloveryPace@users.noreply.github.com>
2023-12-14 17:01:21 +0800

    adminUserHistoryDetail done

--- frontend/src/adminService/adminUserRow.js
+++ frontend/src/adminService/adminUserRow.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { TableRow, TableCell, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
 
 export default function AdminUserRow({ user }) {
+	const navigate = useNavigate();
+
 	const handleViewHistory = (userId) => {
-		console.log(`查看使用者 ${userId} 的歷史紀錄`);
+		console.log(userId);
+		navigate(`/adminUserHistoryDetail?id=${userId}`);
 	};
 
 	const handleModifyUser = (userId) => {
@@ -16,12 +20,16 @@ export default function AdminUserRow({ user }) {
 			<TableCell>{user.email}</TableCell>
 			<TableCell>{user.phone}</TableCell>
 			<TableCell>
-				<Button variant="outlined" onClick={() => handleViewHistory(user.id)}>
+				<Button
+					variant="outlined"
+					onClick={() => handleViewHistory(user.user_id)}>
 					查看歷史紀錄
 				</Button>
 			</TableCell>
 			<TableCell>
-				<Button variant="outlined" onClick={() => handleModifyUser(user.id)}>
+				<Button
+					variant="outlined"
+					onClick={() => handleModifyUser(user.user_id)}>
 					修改資料
 				</Button>
 			</TableCell>
