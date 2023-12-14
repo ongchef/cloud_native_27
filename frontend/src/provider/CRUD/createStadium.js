bda427c52e0093ef6d979e0f05663a3c3b7da1cb
Wonuuuuu <gordon44567@gmail.com>
2023-12-14 14:11:06 +0800

    Blob

--- frontend/src/provider/CRUD/createStadium.js
+++ frontend/src/provider/CRUD/createStadium.js
@@ -20,248 +20,277 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import JButton from '@mui/joy/Button';
-import JToggleButtonGroup from '@mui/joy/ToggleButtonGroup';
-import DeleteIcon from '@mui/icons-material/Delete';
+import JButton from "@mui/joy/Button";
+import JToggleButtonGroup from "@mui/joy/ToggleButtonGroup";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import dayjs from "dayjs";
 import FetchData from "../../authService/fetchData";
 const week = {
-  "mon":"一",
-  "tue":"二",
-  "wed":"三",
-  "thu":"四",
-  "fri":"五",
-  "sat":"六",
-  "sun":"日"
-}
+  mon: "一",
+  tue: "二",
+  wed: "三",
+  thu: "四",
+  fri: "五",
+  sat: "六",
+  sun: "日",
+};
 function checkTimeSeries(newTime) {
-  
-      if (newTime[1].isBefore(newTime[0])) {
-        alert("起始時間與結束時間未按照順序");
-        return false;
-      }
-      else{
-        return true;
-      }
+  if (newTime[1].isBefore(newTime[0])) {
+    alert("起始時間與結束時間未按照順序");
+    return false;
+  } else {
+    return true;
+  }
 }
 function checkTimeOverlap(availableTime) {
-
   for (const [day, daytime] of Object.entries(availableTime)) {
     var timeList = [];
-    for (const time of Object.values(daytime)){
-      timeList.push(time)
+    for (const time of Object.values(daytime)) {
+      timeList.push(time);
     }
     console.log(timeList);
     timeList.sort(function (a, b) {
-    if (a[0].isAfter(b[0])) {
-      return 1;
-    } else {
-      return -1;
+      if (a[0].isAfter(b[0])) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    for (var i = 0; i < timeList.length - 1; i++) {
+      if (timeList[i][1].isAfter(timeList[i + 1][0])) {
+        alert("星期" + week[day] + "時段重疊");
+      }
     }
-  });
-  for (var i = 0; i < timeList.length - 1; i++) {
-    if (timeList[i][1].isAfter(timeList[i + 1][0])) {
-      alert("星期"+week[day]+"時段重疊");
-    }
+    console.log(timeList);
   }
-  console.log(timeList);
-  }
-  
 }
-const EditDialog=(props)=>{
+const EditDialog = (props) => {
   const [open, setOpen] = useState(false);
-  const [newTime, setNewTime] = useState([])
-  const{availableTime,setAvailableTime,selectedDay,id,}=props
+  const [newTime, setNewTime] = useState([]);
+  const { availableTime, setAvailableTime, selectedDay, id } = props;
   const handleClickOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
-    setOpen(false);    
+    setOpen(false);
   };
-  const handleSubmit = () =>{
-    console.log(newTime)
-    if (checkTimeSeries(newTime)){
-      setAvailableTime({...availableTime,[selectedDay]:{...availableTime[selectedDay],[id]:newTime}})
+  const handleSubmit = () => {
+    console.log(newTime);
+    if (checkTimeSeries(newTime)) {
+      setAvailableTime({
+        ...availableTime,
+        [selectedDay]: { ...availableTime[selectedDay], [id]: newTime },
+      });
     }
-    
-    
-    handleClose()
-  }
-  
+
+    handleClose();
+  };
+
   return (
     <>
-    <IconButton variant="outlined" onClick={handleClickOpen}>
+      <IconButton variant="outlined" onClick={handleClickOpen}>
         <EditIcon />
       </IconButton>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>選取日期與時間</DialogTitle>
-        <DialogContent><FormDialog startTime={availableTime[selectedDay][id][0]} endTime={availableTime[selectedDay][id][1]} selectedDay={selectedDay} disabled={true} newTime = {newTime} setNewTime={setNewTime}/></DialogContent>
+        <DialogContent>
+          <FormDialog
+            startTime={availableTime[selectedDay][id][0]}
+            endTime={availableTime[selectedDay][id][1]}
+            selectedDay={selectedDay}
+            disabled={true}
+            newTime={newTime}
+            setNewTime={setNewTime}
+          />
+        </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>取消</Button>
           <Button onClick={handleSubmit}>確認</Button>
         </DialogActions>
       </Dialog>
-      </>
-  )
-}
-const AddDialog=(props)=>{
+    </>
+  );
+};
+const AddDialog = (props) => {
   const [open, setOpen] = useState(false);
-  const [newTime, setNewTime] = useState([])
-  const [selectedDay, setSelectedDay] = useState([])
-  const{availableTime,setAvailableTime}=props
+  const [newTime, setNewTime] = useState([]);
+  const [selectedDay, setSelectedDay] = useState([]);
+  const { availableTime, setAvailableTime } = props;
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
-    setNewTime([])
-    setSelectedDay([])
-    setOpen(false);    
+    setNewTime([]);
+    setSelectedDay([]);
+    setOpen(false);
   };
-  const handleSubmit = () =>{
-    console.log(newTime)
-    console.log(availableTime)
-    console.log(selectedDay)
-    if(checkTimeSeries(newTime)){
-      var newAvailableTime  = availableTime
-      selectedDay.map((day)=>
-        newAvailableTime = {...newAvailableTime,[day]:{...newAvailableTime[day],[Object.keys(newAvailableTime[day]).length]:newTime}}
-      )
-      console.log(newAvailableTime)
-      setAvailableTime(newAvailableTime)
+  const handleSubmit = () => {
+    console.log(newTime);
+    console.log(availableTime);
+    console.log(selectedDay);
+    if (checkTimeSeries(newTime)) {
+      var newAvailableTime = availableTime;
+      selectedDay.map(
+        (day) =>
+          (newAvailableTime = {
+            ...newAvailableTime,
+            [day]: {
+              ...newAvailableTime[day],
+              [Object.keys(newAvailableTime[day]).length]: newTime,
+            },
+          })
+      );
+      console.log(newAvailableTime);
+      setAvailableTime(newAvailableTime);
     }
-  
-    handleClose()
+
+    handleClose();
     // checkTimeOverlap(startTimeList, endTimeList)
     // console.log(startTimeList)
-  }
+  };
   return (
     <>
-    <Button variant="outlined" onClick={handleClickOpen}>
+      <Button variant="outlined" onClick={handleClickOpen}>
         新增開放時間
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>選取日期與時間</DialogTitle>
-        <DialogContent><FormDialog  startTime={dayjs("9:00",'HH:mm')} endTime={dayjs("22:00",'HH:mm')} selectedDay={selectedDay} setSelectedDay={setSelectedDay} disabled={false} newTime = {newTime} setNewTime={setNewTime}/></DialogContent>
+        <DialogContent>
+          <FormDialog
+            startTime={dayjs("9:00", "HH:mm")}
+            endTime={dayjs("22:00", "HH:mm")}
+            selectedDay={selectedDay}
+            setSelectedDay={setSelectedDay}
+            disabled={false}
+            newTime={newTime}
+            setNewTime={setNewTime}
+          />
+        </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>取消</Button>
           <Button onClick={handleSubmit}>確認</Button>
         </DialogActions>
       </Dialog>
-      </>
-  )
-}
-const  FormDialog=(props)=>{
-
+    </>
+  );
+};
+const FormDialog = (props) => {
   const [startTime, setStartTime] = useState([]);
   const [endTime, setEndTime] = useState([]);
   const [periodId, setPeriodId] = useState(0);
-  const {newTime,setNewTime,selectedDay,setSelectedDay} = props
+  const { newTime, setNewTime, selectedDay, setSelectedDay } = props;
 
   useEffect(() => {
-    setStartTime(props.startTime)
-    setEndTime(props.endTime)
-    setNewTime([props.startTime,props.endTime])
+    setStartTime(props.startTime);
+    setEndTime(props.endTime);
+    setNewTime([props.startTime, props.endTime]);
   }, []);
-  useEffect(()=>{
-    console.log(newTime)
-  },[newTime])
+  useEffect(() => {
+    console.log(newTime);
+  }, [newTime]);
   return (
     <>
-        <JToggleButtonGroup
-          value={selectedDay}
-          disabled = {props.disabled}
-          onChange={(event,value)=>{
-            console.log(selectedDay)
-            setSelectedDay(value)}}
-          sx={{display:'flex', justifyContent:'center', borderLeft:'' }}    
-          spacing={3}
-          color="primary"   
-             
-        >
-        {Object.keys(week).map((eng)=>{
-
-          return (
-          <JButton value={eng} >
-            {week[eng]}
-          </JButton >)
+      <JToggleButtonGroup
+        value={selectedDay}
+        disabled={props.disabled}
+        onChange={(event, value) => {
+          console.log(selectedDay);
+          setSelectedDay(value);
+        }}
+        sx={{ display: "flex", justifyContent: "center", borderLeft: "" }}
+        spacing={3}
+        color="primary"
+      >
+        {Object.keys(week).map((eng) => {
+          return <JButton value={eng}>{week[eng]}</JButton>;
         })}
-        </JToggleButtonGroup>
-        
-        <br></br>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['TimePicker', 'TimePicker']}>
-        <TimePicker
-          timeSteps={{ minutes: 30 }}
-          ampm={false}
-          label="起始時段"
-          // views={["hours","minutes"]}
-          format="HH:mm"
-          value={startTime?dayjs(startTime):dayjs("9:00",'HH:mm')}
-          onChange={(value) => {
-            setNewTime([dayjs(value.get("hour") + ":" + value.get("minute"),"HH:m"),newTime[1]])
-          }}
-        />
-        <TimePicker
-          timeSteps={{ minutes: 30 }}
-          ampm={false}
-          label="結束時段"
-          // views={["hours","minutes"]}
-          format="HH:mm"
-          value={endTime?dayjs(endTime):dayjs("22:00",'HH:mm')}
-          // defaultValue={dayjs("0000-00-00T9:00")}
-          onChange={(value) => {
-            setNewTime([newTime[0],dayjs(value.get("hour") + ":" + value.get("minute"),"HH:mm")])
-          }}
-        />
-      </DemoContainer>
-    </LocalizationProvider>
-        
+      </JToggleButtonGroup>
+
+      <br></br>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["TimePicker", "TimePicker"]}>
+          <TimePicker
+            timeSteps={{ minutes: 30 }}
+            ampm={false}
+            label="起始時段"
+            // views={["hours","minutes"]}
+            format="HH:mm"
+            value={startTime ? dayjs(startTime) : dayjs("9:00", "HH:mm")}
+            onChange={(value) => {
+              setNewTime([
+                dayjs(value.get("hour") + ":" + value.get("minute"), "HH:m"),
+                newTime[1],
+              ]);
+            }}
+          />
+          <TimePicker
+            timeSteps={{ minutes: 30 }}
+            ampm={false}
+            label="結束時段"
+            // views={["hours","minutes"]}
+            format="HH:mm"
+            value={endTime ? dayjs(endTime) : dayjs("22:00", "HH:mm")}
+            // defaultValue={dayjs("0000-00-00T9:00")}
+            onChange={(value) => {
+              setNewTime([
+                newTime[0],
+                dayjs(value.get("hour") + ":" + value.get("minute"), "HH:mm"),
+              ]);
+            }}
+          />
+        </DemoContainer>
+      </LocalizationProvider>
     </>
   );
-}
+};
 
 export default function CreateStadium() {
   const navigate = useNavigate();
   const [image, setImage] = useState();
-  const [ court, setCourt] = useState({});
+  const [court, setCourt] = useState({});
   // const [imgblob, setImgBlob] = useState();
-  const [ availableTime, setAvailableTime] = useState({
-    mon:{0:[dayjs("9:00",'HH:mm'),dayjs("11:00",'HH:mm')],
-    1:[dayjs("13:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    tue:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    wed:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    thu:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    fri:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    sat:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]},
-    sun:{0:[dayjs("9:00",'HH:mm'),dayjs("22:00",'HH:mm')]}
-  })
-  function handleSubmit(){
-    checkTimeOverlap(availableTime)
-    const stadium={
-
-    }
-    console.log(court)
-    console.log(availableTime)
-    console.log(image)
-    FetchData.postDateWithImg("http://localhost:3000/api/courts",court,image)
+  const [availableTime, setAvailableTime] = useState({
+    mon: {
+      0: [dayjs("9:00", "HH:mm"), dayjs("11:00", "HH:mm")],
+      1: [dayjs("13:00", "HH:mm"), dayjs("22:00", "HH:mm")],
+    },
+    tue: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+    wed: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+    thu: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+    fri: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+    sat: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+    sun: { 0: [dayjs("9:00", "HH:mm"), dayjs("22:00", "HH:mm")] },
+  });
+  function handleSubmit() {
+    checkTimeOverlap(availableTime);
+    const stadium = {};
+    console.log(court);
+    console.log(availableTime);
+    console.log(image);
+    FetchData.postDateWithImg("http://localhost:3000/api/courts", court, image);
   }
   const handleChange = (e) => {
-    setCourt({...court,[e.target.id]:e.target.value})
-  }
+    setCourt({ ...court, [e.target.id]: e.target.value });
+  };
   const handleImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
       let img = URL.createObjectURL(e.target.files[0]);
       setImage(img);
-      // setImgBlob(e.target.files[0])
+      fetch(img)
+        .then((response) => response.blob())
+        .then((blob) => {
+          // Now you can use blob
+          const imgblob = blob;
+        })
+        .catch((error) => console.error(error));
     }
   };
-  function handleDelete (day,id){
-
-    delete availableTime[day][id]
-    console.log(availableTime)
-    setAvailableTime({...availableTime})
+  function handleDelete(day, id) {
+    delete availableTime[day][id];
+    console.log(availableTime);
+    setAvailableTime({ ...availableTime });
   }
   function showMyImage(fileInput) {
     var files = fileInput.files;
@@ -277,9 +306,9 @@ export default function CreateStadium() {
       //$("#banner_name").text(file.name);
     }
   }
-  useEffect(()=>{
-    console.log(availableTime)
-  },[availableTime])
+  useEffect(() => {
+    console.log(availableTime);
+  }, [availableTime]);
 
   return (
     <div>
@@ -302,7 +331,9 @@ export default function CreateStadium() {
             <Card sx={{ width: "70vw", margin: "auto" }}>
               <Grid container>
                 <Grid item xs={6}>
-                  {image&&<CardMedia component="img" src={image} alt="Stadium" />}
+                  {image && (
+                    <CardMedia component="img" src={image} alt="Stadium" />
+                  )}
                   <TextField
                     type="file"
                     onChange={(e) => {
@@ -317,48 +348,58 @@ export default function CreateStadium() {
                 <Grid item xs={6}>
                   <CardContent>
                     <Typography>球場名稱</Typography>
-                    <TextField 
+                    <TextField
                       id="name"
                       fullWidth
                       size="small"
-                      onChange={handleChange} />
+                      onChange={handleChange}
+                    />
                     <Typography>球場地點</Typography>
-                    <TextField 
-                      id='location'
+                    <TextField
+                      id="location"
                       fullWidth
                       size="small"
-                      onChange={handleChange} />
+                      onChange={handleChange}
+                    />
                     <Typography>球場地址</Typography>
                     <TextField
-                      id='address'
+                      id="address"
                       fullWidth
                       size="small"
-                      onChange={handleChange} />
+                      onChange={handleChange}
+                    />
                     <Typography>最大使用人數</Typography>
-                    <TextField 
-                      id='available'
+                    <TextField
+                      id="available"
                       fullWidth
                       size="small"
-                      onChange={handleChange} />
-                    {Object.keys(availableTime).map((day)=>{
-                      return(
-                      Object.keys(availableTime[day]).map((id)=>{
+                      onChange={handleChange}
+                    />
+                    {Object.keys(availableTime).map((day) => {
+                      return Object.keys(availableTime[day]).map((id) => {
                         return (
-                        <Typography>
-                          星期{week[day]}{availableTime[day][id][0].format('HH:mm')}-{availableTime[day][id][1].format('HH:mm')}
-                        <EditDialog availableTime={availableTime} setAvailableTime={setAvailableTime} id={id} selectedDay={day}/>
-                        <IconButton onClick={()=>handleDelete(day,id)}>
-                          <DeleteIcon />
-                        </IconButton>
-                        </Typography>
-                        )
-                      })
-                      )
+                          <Typography>
+                            星期{week[day]}
+                            {availableTime[day][id][0].format("HH:mm")}-
+                            {availableTime[day][id][1].format("HH:mm")}
+                            <EditDialog
+                              availableTime={availableTime}
+                              setAvailableTime={setAvailableTime}
+                              id={id}
+                              selectedDay={day}
+                            />
+                            <IconButton onClick={() => handleDelete(day, id)}>
+                              <DeleteIcon />
+                            </IconButton>
+                          </Typography>
+                        );
+                      });
                     })}
-                    <AddDialog availableTime={availableTime} setAvailableTime={setAvailableTime}/>
-                    <Button onClick={handleSubmit}>
-                      送出
-                    </Button>
+                    <AddDialog
+                      availableTime={availableTime}
+                      setAvailableTime={setAvailableTime}
+                    />
+                    <Button onClick={handleSubmit}>送出</Button>
                   </CardContent>
                 </Grid>
               </Grid>
