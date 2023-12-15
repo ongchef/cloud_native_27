6c590b152031abc8e8e5b804c59ae1efe1aad8fa
Wonuuuuu <gordon44567@gmail.com>
2023-12-15 17:53:15 +0800

    loading

--- frontend/src/userService/joinStadiumDetail.js
+++ frontend/src/userService/joinStadiumDetail.js
@@ -5,7 +5,7 @@ import Container from "@mui/material/Container"; // 引入Container元件
 import Box from "@mui/material/Box"; // 引入Box元件
 import pic from "../pic/羽球1.png";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Card } from "@mui/material";
+import { Card, CircularProgress } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -27,6 +27,7 @@ export default function JoinStadiumDetail() {
   const id = searchParams.get("id");
   const [date, setDate] = useState();
   const [password, setPassword] = useState();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     let url = new URL(window.location.href);
     let params = url.searchParams;
@@ -50,7 +51,11 @@ export default function JoinStadiumDetail() {
     );
   }
   useEffect(() => {
-    getStadiumDetail().then((res) => setAppointmentDetail(res.data[0]));
+    setLoading(true);
+    getStadiumDetail().then((res) => {
+      setAppointmentDetail(res.data[0]);
+      setLoading(false);
+    });
   }, []);
   useEffect(() => {
     console.log(appointmentDetail);
@@ -112,150 +117,154 @@ export default function JoinStadiumDetail() {
           {/* 內容1 */}
         </Container>
         <Container maxWidth="sm">
-          <Box my={2}>
-            <Card sx={{ width: "70vw", margin: "auto" }}>
-              <Grid
-                paddingLeft="30px"
-                container
-                spacing={2}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Grid item xs={12} sm={6}>
-                  <CardMedia
-                    component="img"
-                    image={
-                      appointmentDetail.image_url.split(".jpg")[0] + ".jpg"
-                    } // 替換為您的圖片URL
-                    alt="Stadium"
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <CardContent>
-                    <Typography
-                      variant="h5"
-                      component="div"
-                      sx={{ fontWeight: "bold" }}
-                    >
-                      {appointmentDetail.court_name} -{" "}
-                      {appointmentDetail.location} {date}
-                    </Typography>
-                    <Box
-                      mx={1}
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      flexWrap="wrap"
-                      gap={1}
-                    >
-                      <Typography variant="body2" color="text.secondary">
-                        {appointmentDetail.address}
-                      </Typography>
-                      <Typography variant="body2" color="000000">
-                        <span
-                          style={{
-                            color: "000000",
-                            backgroundColor: "#D9D9D9",
-                            paddingLeft: "10px",
-                            paddingRight: "10px",
-                            fontSize: "14px",
-                          }}
-                        >
-                          {date}{" "}
-                          {appointmentDetail.start_time
-                            ? appointmentDetail.start_time.substring(0, 5)
-                            : " "}
-                          ~
-                          {appointmentDetail.end_time
-                            ? appointmentDetail.end_time.substring(0, 5)
-                            : " "}
-                        </span>
-                      </Typography>
-                      <Typography variant="body2" color="000000">
-                        {(appointmentTag || []).map((tag, index) => (
-                          <React.Fragment key={index}>
-                            <span
-                              style={{
-                                color: "000000",
-                                backgroundColor: "#D9D9D9",
-                                paddingLeft: "10px",
-                                paddingRight: "10px",
-                                paddingTop: "5px",
-                                paddingBottom: "5px",
-                                fontSize: "16px",
-                              }}
-                            >
-                              {tag}
-                            </span>
-                            {index < tags.length - 1 && <>&nbsp;</>}
-                          </React.Fragment>
-                        ))}
-                      </Typography>
-                      <Typography variant="body2" color="000000">
-                        當前場地預約人數：{" "}
-                        <strong>
-                          {appointmentDetail.attendence}/
-                          {appointmentDetail.available}
-                        </strong>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <Box my={2}>
+              <Card sx={{ width: "70vw", margin: "auto" }}>
+                <Grid
+                  paddingLeft="30px"
+                  container
+                  spacing={2}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Grid item xs={12} sm={6}>
+                    <CardMedia
+                      component="img"
+                      image={
+                        appointmentDetail.image_url.split(".jpg")[0] + ".jpg"
+                      } // 替換為您的圖片URL
+                      alt="Stadium"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <CardContent>
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        sx={{ fontWeight: "bold" }}
+                      >
+                        {appointmentDetail.court_name} -{" "}
+                        {appointmentDetail.location} {date}
                       </Typography>
                       <Box
+                        mx={1}
                         display="flex"
-                        flexDirection="row"
-                        alignItems="center"
+                        flexDirection="column"
+                        justifyContent="center"
                         flexWrap="wrap"
                         gap={1}
                       >
-                        <Avatar
-                          sx={{ bgcolor: "#FFA370" }}
-                          alt="Remy Sharp"
-                          src="/broken-image.jpg"
-                        >
-                          {appointmentDetail.creator_name
-                            ? appointmentDetail.creator_name[0]
-                            : " "}
-                        </Avatar>
-                        主揪人：{appointmentDetail.creator_name}
-                      </Box>
-                      {publicIndex === 0 ? (
-                        <Box my={1} display="flex" alignItems="center">
-                          <Typography
-                            variant="body1"
-                            color="000000"
-                            paddingX={1}
-                            paddingY={0.2}
-                            sx={{ fontWeight: "bold" }}
+                        <Typography variant="body2" color="text.secondary">
+                          {appointmentDetail.address}
+                        </Typography>
+                        <Typography variant="body2" color="000000">
+                          <span
+                            style={{
+                              color: "000000",
+                              backgroundColor: "#D9D9D9",
+                              paddingLeft: "10px",
+                              paddingRight: "10px",
+                              fontSize: "14px",
+                            }}
                           >
-                            密碼：
-                          </Typography>
-
-                          <Input
-                            type="password"
-                            style={{ width: 200 }}
-                            onChange={(e) => setPassword(e.target.value)}
-                          />
-                        </Box>
-                      ) : (
-                        <></>
-                      )}
-                      <Box display="flex" justifyContent="flex-end">
-                        <Button
-                          width="300px"
-                          variant="outlined"
-                          onClick={() => {
-                            joinAppointment();
-                            console.log("加入場地api");
-                          }}
+                            {date}{" "}
+                            {appointmentDetail.start_time
+                              ? appointmentDetail.start_time.substring(0, 5)
+                              : " "}
+                            ~
+                            {appointmentDetail.end_time
+                              ? appointmentDetail.end_time.substring(0, 5)
+                              : " "}
+                          </span>
+                        </Typography>
+                        <Typography variant="body2" color="000000">
+                          {(appointmentTag || []).map((tag, index) => (
+                            <React.Fragment key={index}>
+                              <span
+                                style={{
+                                  color: "000000",
+                                  backgroundColor: "#D9D9D9",
+                                  paddingLeft: "10px",
+                                  paddingRight: "10px",
+                                  paddingTop: "5px",
+                                  paddingBottom: "5px",
+                                  fontSize: "16px",
+                                }}
+                              >
+                                {tag}
+                              </span>
+                              {index < tags.length - 1 && <>&nbsp;</>}
+                            </React.Fragment>
+                          ))}
+                        </Typography>
+                        <Typography variant="body2" color="000000">
+                          當前場地預約人數：{" "}
+                          <strong>
+                            {appointmentDetail.attendence}/
+                            {appointmentDetail.available}
+                          </strong>
+                        </Typography>
+                        <Box
+                          display="flex"
+                          flexDirection="row"
+                          alignItems="center"
+                          flexWrap="wrap"
+                          gap={1}
                         >
-                          <ArrowForwardIcon />
-                          加入場地
-                        </Button>
+                          <Avatar
+                            sx={{ bgcolor: "#FFA370" }}
+                            alt="Remy Sharp"
+                            src="/broken-image.jpg"
+                          >
+                            {appointmentDetail.creator_name
+                              ? appointmentDetail.creator_name[0]
+                              : " "}
+                          </Avatar>
+                          主揪人：{appointmentDetail.creator_name}
+                        </Box>
+                        {publicIndex === 0 ? (
+                          <Box my={1} display="flex" alignItems="center">
+                            <Typography
+                              variant="body1"
+                              color="000000"
+                              paddingX={1}
+                              paddingY={0.2}
+                              sx={{ fontWeight: "bold" }}
+                            >
+                              密碼：
+                            </Typography>
+
+                            <Input
+                              type="password"
+                              style={{ width: 200 }}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </Box>
+                        ) : (
+                          <></>
+                        )}
+                        <Box display="flex" justifyContent="flex-end">
+                          <Button
+                            width="300px"
+                            variant="outlined"
+                            onClick={() => {
+                              joinAppointment();
+                              console.log("加入場地api");
+                            }}
+                          >
+                            <ArrowForwardIcon />
+                            加入場地
+                          </Button>
+                        </Box>
                       </Box>
-                    </Box>
-                  </CardContent>
+                    </CardContent>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Card>
-          </Box>
+              </Card>
+            </Box>
+          )}
         </Container>
       </Box>
       {/* 添加這一行 */}
