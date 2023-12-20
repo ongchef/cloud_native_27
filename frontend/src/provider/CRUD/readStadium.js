82e8bad8ad7690e884035157b14101b095d543e4
Wonuuuuu <gordon44567@gmail.com>
2023-12-20 18:20:20 +0800

    UI

--- frontend/src/provider/CRUD/readStadium.js
+++ frontend/src/provider/CRUD/readStadium.js
@@ -29,42 +29,29 @@ export default function ReadStadium() {
   const [sport, setSport] = useState(10);
   const [address, setAddress] = useState(20);
   const [date, setDate] = useState(moment(new Date()).format("YYYY-MM-DD"));
-  const [courtList, setCourtList] = useState([])
+  const [courtList, setCourtList] = useState([]);
   const [weekday, setWeekday] = useState(moment(date).day());
-  const [courtName, setCourtName] = useState()
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(1)
-  const [totalPage, setTotalPage] = useState(0)
-  const weekdayMapping = [
-    "日",
-    "一",
-    "二",
-    "三",
-    "四",
-    "五",
-    "六",
-    "日",
-  ];
-  async function SearchCourt(courtName,sport,address){
-    setLoading(true)
-    return FetchData.getData("api/courts/admin",1,
-    {
-      ...(courtName&& courtName && {name:courtName}),
-      ...(sport&& sport && {ball:sport}),
-      ...(address&& address && {address:address}),
-    })
-    .then((res)=>
-      {
-        setCourtList(res.courts)
-        setTotalPage(res.total_page)
-        setLoading(false)
-      }
-    )
+  const [courtName, setCourtName] = useState();
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const weekdayMapping = ["日", "一", "二", "三", "四", "五", "六", "日"];
+  async function SearchCourt(courtName, sport, address) {
+    setLoading(true);
+    return FetchData.getData("api/courts/admin", 1, {
+      ...(courtName && courtName && { name: courtName }),
+      ...(sport && sport && { ball: sport }),
+      ...(address && address && { address: address }),
+    }).then((res) => {
+      setCourtList(res.courts);
+      setTotalPage(res.total_page);
+      setLoading(false);
+    });
   }
   useEffect(() => {
     fakeStadium();
-    SearchCourt()
-  },[]);
+    SearchCourt();
+  }, []);
   const handleSportChange = (event) => {
     setSport(event.target.value);
   };
@@ -74,139 +61,145 @@ export default function ReadStadium() {
   };
   return (
     <div>
-      <h1>Order Stadium</h1>
-      {
-      loading?
-      <Box >
-      <LinearProgress sx={{display:'flex', justifyContent:'center'}}/>
-      </Box>
-      :
-      <>
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-        width="70vw"
-        margin="auto"
-      >
-        <Box m={1}>
-          <FormControl>
-          {/* <InputLabel>球場名稱</InputLabel> */}
-          <TextField
-            placeholder="球場名稱"
-            label="球場名稱"
-            value={courtName}
-            onClick={(event, value)=>setCourtName(value)}
+      <h2>Order Stadium</h2>
+      {loading ? (
+        <Box>
+          <LinearProgress sx={{ display: "flex", justifyContent: "center" }} />
+        </Box>
+      ) : (
+        <>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            width="70vw"
+            margin="auto"
           >
+            <Box m={0.5}>
+              <FormControl>
+                {/* <InputLabel>球場名稱</InputLabel> */}
+                <TextField
+                  placeholder="球場名稱"
+                  label="球場名稱"
+                  value={courtName}
+                  onClick={(event, value) => setCourtName(value)}
+                ></TextField>
+              </FormControl>
+            </Box>
+            <Box m={1}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">球類</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={sport}
+                  onChange={handleSportChange}
+                  label="球類"
+                  style={{ width: "80px" }}
+                >
+                  <MenuItem value={undefined}>所有球類</MenuItem>
+                  <MenuItem value={"1"}>羽球</MenuItem>
+                  <MenuItem value={"2"}>籃球</MenuItem>
+                  <MenuItem value={"3"}>排球</MenuItem>
+                  <MenuItem value={"4"}>桌球</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box m={1}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label-2">
+                  Location
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label-2"
+                  id="demo-simple-select-2"
+                  value={address}
+                  onChange={handleAddressChange}
+                  label="Address"
+                  style={{ width: "100px" }}
+                >
+                  <MenuItem value={undefined}>所有地區</MenuItem>
+                  <MenuItem value={"大安區"}>大安區</MenuItem>
+                  <MenuItem value={"文山區"}>文山區</MenuItem>
+                  <MenuItem value={"信義區"}>信義區</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box m={1}>
+              <Button
+                variant="contained"
+                onClick={() => SearchCourt(courtName, sport, address)}
+              >
+                Search
+              </Button>
+            </Box>
+          </Box>
+          <Box>
+            <Grid container>
+              <Grid item xs={1.5}></Grid>
+              <Grid item xs={4.5}>
+                <Typography variant="h4" color="text.secondary">
+                  球場預約情況
+                </Typography>
+              </Grid>
+              <Grid item xs={3}></Grid>
+              <Grid item xs={3}>
+                <Button
+                  variant="outlined"
+                  startIcon={<AddIcon />}
+                  sx={{ lineHeight: 0 }}
+                  onClick={() => (window.location.href = "/createStadium")}
+                >
+                  新增球場
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box m={0.5} sx={{ height: "70vh", overflowY: "auto" }}>
+            {courtList &&
+              courtList.map((court) => {
+                const weekdayInChinese = weekdayMapping[weekday];
 
-          </TextField>
-          </FormControl>
-        </Box>
-        <Box m={1}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">球類</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={sport}
-              onChange={handleSportChange}
-              label="球類"
-              style={{ width: "80px" }}
-            >
-              <MenuItem value={undefined}>所有球類</MenuItem>
-              <MenuItem value={"1"}>羽球</MenuItem>
-              <MenuItem value={"2"}>籃球</MenuItem>
-              <MenuItem value={"3"}>排球</MenuItem>
-              <MenuItem value={"4"}>桌球</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Box m={1}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label-2">Location</InputLabel>
-            <Select
-              labelId="demo-simple-select-label-2"
-              id="demo-simple-select-2"
-              value={address}
-              onChange={handleAddressChange}
-              label="Address"
-              style={{ width: "100px" }}
-            >
-              <MenuItem value={undefined}>所有地區</MenuItem>
-              <MenuItem value={"大安區"}>大安區</MenuItem>
-              <MenuItem value={"文山區"}>文山區</MenuItem>
-              <MenuItem value={"信義區"}>信義區</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        <Box m={1}>
-          <Button variant="contained" onClick={()=>SearchCourt(courtName,sport,address)}>Search</Button>
-        </Box>
-      </Box>
-      <Box>
-        <Grid container>
-          <Grid item xs={1.5}></Grid>
-          <Grid item xs={4.5}>
-            <Typography variant="h4" color="text.secondary">
-              球場預約情況
-            </Typography>
-          </Grid>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={3}>
-            <Button
-              variant="outlined"
-              startIcon={<AddIcon />}
-              sx={{ lineHeight: 0 }}
-              onClick={()=>window.location.href="/createStadium"}
-            >
-              新增球場
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-      <Box m={0.5} sx={{ height: "70vh", overflowY: "auto" }}>
-      {
-        courtList&&
-          courtList.map((court)=>{
-            const weekdayInChinese = weekdayMapping[weekday];
-
-            const availableTime = court.available_time.find(
-              (time) => time.weekday === weekday
-            );
-            const startTime = availableTime
-              ? availableTime.start_time.substring(0, 5)
-              : "";
-            const endTime = availableTime
-              ? availableTime.end_time.substring(0, 5)
-              : "";
-            return(
-            <StadiumCard
-              id={court.court_id}
-              image={court.image_url&&court.image_url.split(".jpg")[0]+".jpg"}
-              title={court.name + " - " + court.location}
-
-              description={[
-                court.address,
-                "週" + weekdayInChinese,
-                startTime + "~" + endTime,
-                court.available,
-              ]}
-            />)}
-          )
-        }
-        
-      </Box>
-      <Box display="flex" justifyContent="center" marginTop="20px">
-        {/* 其他內容 */}
-        <Pagination
-          count={totalPage}
-          onChange={(event,num)=>setPage(num)} 
-          page={page}
-          color="primary" /> {/* 添加這一行 */}
-      </Box>
-      </>
-    }
+                const availableTime = court.available_time.find(
+                  (time) => time.weekday === weekday
+                );
+                const startTime = availableTime
+                  ? availableTime.start_time.substring(0, 5)
+                  : "";
+                const endTime = availableTime
+                  ? availableTime.end_time.substring(0, 5)
+                  : "";
+                return (
+                  <StadiumCard
+                    id={court.court_id}
+                    image={
+                      court.image_url &&
+                      court.image_url.split(".jpg")[0] + ".jpg"
+                    }
+                    title={court.name + " - " + court.location}
+                    description={[
+                      court.address,
+                      "週" + weekdayInChinese,
+                      startTime + "~" + endTime,
+                      court.available,
+                    ]}
+                  />
+                );
+              })}
+          </Box>
+          <Box display="flex" justifyContent="center" marginTop="20px">
+            {/* 其他內容 */}
+            <Pagination
+              count={totalPage}
+              onChange={(event, num) => setPage(num)}
+              page={page}
+              color="primary"
+            />{" "}
+            {/* 添加這一行 */}
+          </Box>
+        </>
+      )}
     </div>
   );
 }
